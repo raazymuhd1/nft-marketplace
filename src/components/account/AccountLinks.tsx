8f4dfc30e55b8e8691327a55c585d11f13ed72ef
raazymuhd1 <muhammadrazi51@gmail.com>
2025-02-12 19:59:22 +0700

    working on a user social links

--- src/components/account/AccountLinks.tsx
+++ src/components/account/AccountLinks.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FaGlobe } from "react-icons/fa";
 
 const AccountLinks = () => {
   return (
     <div className="flex flex-col gap-[10px]">
-        <h3> Add Links </h3>
+        <h3 className='text-[1vmax] font-bold'> Add Links </h3>
         <div className='min-h-[200px] border-[1px] border-secondaryAlt radius p-[20px]'>
+            <aside className="flex-row-center gap-[10px] w-full border-[1px] border-secondaryAlt  p-[3px] radius">
+                <FaGlobe size={16} className="h-[60%] border-[1px]  border-secondaryAlt radius w-[5%]" />
+                <input type="text" name="" id="" placeholder='your website' className="p-[10px] border-l-[1px] border-secondaryAlt outline-none w-[90%] h-full placeholder:font-bold" />
+            </aside>
 
         </div>
     </div>
--- src/components/account/EditAccount.tsx
+++ src/components/account/EditAccount.tsx
@@ -7,7 +7,7 @@ import AccountLinks from './AccountLinks'
 const EditAccount = () => {
   return (
     <div className="w-full">
-        <div className='border-b-[1px] border-secondaryAlt h-[100px] sticky top-0 p-[20px] w-full'>
+        <div className='border-b-[1px] border-secondaryAlt h-[100px] p-[20px] w-full'>
             <h2 className="font-bold text-[20px] md:text-[30px] lg:text-[35px] text-center w-full"> Edit Personal Information </h2>
         </div>
 
